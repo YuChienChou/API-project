543ec4f9165472c0d98fbd46bf31c4df7c5bb39c
Leslie Chou <chouyuchien@gmail.com>
2023-04-13 19:09:11 -0700

    Add User login backend endpoint

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -1,9 +1,7 @@
 'use strict';
 
 const { Model, Validator } = require('sequelize');
-const {
-  Model
-} = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
