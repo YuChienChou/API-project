7c9c8895a97d111b53d62bee5964b9ea1a93c2ab
Leslie Chou <chouyuchien@gmail.com>
2023-04-19 13:25:47 -0700

    completed Create a Spot endpoint and fix Get All Spot endpoint bug after new spots are created

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,20 +1,69 @@
 const express = require('express');
 const { Sequelize } = require('sequelize');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 
 const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth');
+
 const { Spot, SpotImage, Review, User } = require('../../db/models');
 const router = express.Router();
 
+const validateSpot = [
+    check('address')
+      .exists({ checkFalsy: true })
+      .withMessage('Street address is required'),
+    check('city')
+      .exists({ checkFalsy: true })
+      .withMessage('City is required'),
+    check('state')
+      .exists({ checkFalsy: true })
+      .withMessage('State is required'),
+    check('country')
+      .exists({ checkFalsy: true })
+      .withMessage('Country is required'),
+    check('lat')
+      .exists({ checkFalsy: true })
+      .isDecimal({force_decimal: true})
+      .withMessage('Latitude is not valid'),
+    check('lng')
+      .exists({ checkFalsy: true })
+      .isDecimal({force_decimal: true})
+      .withMessage('Longitude is not valid'),
+    check('name')
+      .exists({ checkFalsy: true })
+      .isLength({max: 50})
+      .withMessage('Name must be less than 50 characters'),
+    check('description')
+      .exists({ checkFalsy: true })
+      .withMessage('Description is required'),
+    check('price')
+      .exists({ checkFalsy: true })
+      .withMessage('Price per day is required'),
+    handleValidationErrors
+  ];
+
+//get all spots
 router.get('/', async (req, res, next) => {
     const spots = await Spot.findAll();
     const payload = [];
 
     for(let i = 0; i <spots.length; i++) {
-        const spot = spots[i];
+        let spot = spots[i];
 
-        const previewImage = await SpotImage.findByPk(spot.id, {
-            attributes: ['url']
+        let previewImage = await SpotImage.findOne({
+            where: {
+                spotId: spot.id,
+                preview: true},
+            attributes: ['url'],
         });
+
+
+        if(previewImage) {
+            previewImage = previewImage.url;
+        } else {
+            previewImage = null;
+        }
+
 
         const totalRating = await Review.sum('stars', {
             where: {
@@ -45,7 +94,7 @@ router.get('/', async (req, res, next) => {
             createdAt: spot.createdAt,
             updatedAt: spot.updatedAt,
             avgRating,
-            previewImage: previewImage.url,
+            previewImage: previewImage,
             
         };
         payload.push(spotData);
@@ -56,6 +105,7 @@ router.get('/', async (req, res, next) => {
     });
 });
 
+//Get all Spots owned by the Current User
 router.get('/current', requireAuth, async(req, res, next) => {
    const spots = await Spot.findAll({
     where: {
@@ -98,7 +148,7 @@ router.get('/current', requireAuth, async(req, res, next) => {
         createdAt: spot.createdAt,
         updatedAt: spot.updatedAt,
         avgRating,
-        previewImage: previewImage.url,
+        previewImage: previewImage,
     };
 
     payload.push(spotData);
@@ -110,6 +160,7 @@ router.get('/current', requireAuth, async(req, res, next) => {
    })
 });
 
+//Get details of a Spot from an id
 router.get('/:id', async (req, res, next) => {
     const spotId = req.params.id;
     const spot = await Spot.findByPk(spotId);
@@ -118,7 +169,6 @@ router.get('/:id', async (req, res, next) => {
         const err = new Error('Spot couldn\'t be found');
         err.status = 404;
 
-        err.errors = { credential: 'The provided credentials were invalid.' };
         return next(err);
     }
 
@@ -179,9 +229,25 @@ router.get('/:id', async (req, res, next) => {
     });
 } );
 
-// router.post('/', requireAuth, async (req, res, next) => {
+//Create a Spot
+router.post('/', requireAuth, validateSpot, async (req, res, next) => {
+    const { address, city, state, country, lat, lng, name, description, price } = req.body;
 
-// })
+    const newSpot = await Spot.create({
+        ownerId: req.user.id,
+        address, 
+        city, 
+        state, 
+        country, 
+        lat, 
+        lng,
+        name, 
+        description, 
+        price,
+    });
+
+    return res.status(201).json(newSpot);
+})
 
 
 
