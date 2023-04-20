56851d08aab749b8c3ce73417a0442869ae351f1
Leslie Chou <chouyuchien@gmail.com>
2023-04-20 10:44:39 -0700

    completed Get All Current User Bookings endpoint

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -1,13 +1,48 @@
 const express = require('express');
 
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
-const { User } = require('../../db/models');
+const { User, Spot, Booking, SpotImage } = require('../../db/models');
 
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 
 const router = express.Router();
 
+
+//Get all of the Current User's Bookings
+router.get('/current', requireAuth, async(req, res, next) => {
+    const bookings = await Booking.findAll({
+        where: {
+            userId: req.user.id
+        },
+        include: [
+            {model: Spot, attributes: ['id', 'ownerId', 'address', 'city', 'state', 'country', 'lat', 'lng', 'name', 'price']}]
+    });
+
+    console.log(bookings);
+
+    for (let i = 0; i < bookings.length; i++) {
+
+        const booking = bookings[i];
+        const spot = booking.Spot;
+
+        // console.log(spot); //check it in the terminal!!!!
+
+        let previewImage = await SpotImage.findOne({where: {spotId: spot.id
+        }});
+
+        if(previewImage) {
+            previewImage = previewImage.url
+        } else {
+            previewImage = null
+        };
+
+        spot.dataValues.previewImage = previewImage; //check in the terminal fo rthe previewImage!!!
+
+    };
+
+    return res.status(200).json({Bookings: bookings});
+});
 
 
 
