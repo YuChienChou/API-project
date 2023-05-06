a480d11eebe51849a2abd44e5a17bd3eb5b5135b
Leslie Chou <chouyuchien@gmail.com>
2023-05-05 20:38:31 -0700

    completed profileButton without styling

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -1,23 +1,41 @@
 import React, { useState } from "react";
+import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import * as sessionActions from '../../store/session';
 
 function ProfileButton({ user }) {
   const dispatch = useDispatch();
+  const [showMenu, setShowMenu] = useState(false);
+
+  
+  useEffect(() => {
+    const closeMenu = (e) => {
+      setShowMenu(false);
+    };
+
+    document.addEventListener('click', closeMenu);
+
+    return () => document.removeEventListener("click", closeMenu);
+  }, []);
+
+  const openMenu = () => {
+    if (showMenu) return;
+    setShowMenu(true);
+  };
 
   const logout = (e) => {
     e.preventDefault();
     dispatch(sessionActions.logout());
   };
 
-  const ulClassName = "profile-dropdown";
+  const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
 
   return (
     <>
-      <button>
+      <button onClick={() => setShowMenu(!showMenu)}>
         <i className="fas fa-user-circle" />
       </button>
-      <ul className="profile-dropdown">
+      <ul className={ulClassName}>
         <li>{user.username}</li>
         <li>{user.firstName} {user.lastName}</li>
         <li>{user.email}</li>
