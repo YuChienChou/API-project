7bf739c3b647bf6abc20219dc93fb71fa194d8a3
Leslie Chou <chouyuchien@gmail.com>
2023-05-07 16:02:31 -0700

    completed profile session bonus part

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,50 +1,78 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
-import { useSelector } from "react-redux";
-import ProfileButton from "./ProfileButton";
-import OpenModalButton from "../OpenModalButton";
-import LoginFormModal from "../LoginFormModal";
-import SignupFormModal from "../SignupFormModal";
-import "./Navigation.css";
+// import React from "react";
+// import { NavLink } from "react-router-dom";
+// import { useSelector } from "react-redux";
+// import ProfileButton from "./ProfileButton";
+// import OpenModalButton from "../OpenModalButton";
+// import LoginFormModal from "../LoginFormModal";
+// import SignupFormModal from "../SignupFormModal";
+// import "./Navigation.css";
+// import Airbnb from './Airbnb-logo.jpg'
+
+
+
+// function Navigation({ isLoaded }) {
+//   const sessionUser = useSelector((state) => state.session.user);
+
+//   let sessionLinks;
+//   if (sessionUser) {
+//     sessionLinks = (
+//       <li className='profile-session'>
+//         <ProfileButton user={sessionUser} />
+//       </li>
+//     );
+//   } else {
+//     sessionLinks = (
+//         <li>
+//           <OpenModalButton
+//             buttonText="Log In"
+//             modalComponent={<LoginFormModal />}
+//             id='nav-link-button'
+//           />
+//           <OpenModalButton
+//             buttonText="Sign Up"
+//             modalComponent={<SignupFormModal />}
+//             id='nav-link-button'
+//           />
+//         </li>
+//     );
+//   }
+
+//   return (
+//     <ul className='nav-ul'>
+//       <li>
+//         <NavLink exact to="/">
+//         <img src={Airbnb} alt='Airbnb logo' className='logo-img' />
+//         </NavLink>
+//       </li>
+//       {isLoaded && sessionLinks}
+//     </ul>
+//   );
+// }
+
+// export default Navigation;
+
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import ProfileButton from './ProfileButton';
+import './Navigation.css';
 import Airbnb from './Airbnb-logo.jpg'
 
-
-
-function Navigation({ isLoaded }) {
-  const sessionUser = useSelector((state) => state.session.user);
-
-  let sessionLinks;
-  if (sessionUser) {
-    sessionLinks = (
-      <li className='profile-session'>
-        <ProfileButton user={sessionUser} />
-      </li>
-    );
-  } else {
-    sessionLinks = (
-        <li>
-          <OpenModalButton
-            buttonText="Log In"
-            modalComponent={<LoginFormModal />}
-            id='nav-link-button'
-          />
-          <OpenModalButton
-            buttonText="Sign Up"
-            modalComponent={<SignupFormModal />}
-            id='nav-link-button'
-          />
-        </li>
-    );
-  }
+function Navigation({ isLoaded }){
+  const sessionUser = useSelector(state => state.session.user);
 
   return (
     <ul className='nav-ul'>
       <li>
         <NavLink exact to="/">
-        <img src={Airbnb} alt='Airbnb logo' className='logo-img' />
+          <img src={Airbnb} className='logo-img' />
         </NavLink>
       </li>
-      {isLoaded && sessionLinks}
+      {isLoaded && (
+        <li>
+          <ProfileButton user={sessionUser} />
+        </li>
+      )}
     </ul>
   );
 }
