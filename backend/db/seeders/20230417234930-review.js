3d9c09bf78924ef853ef2096378e473f987b27d7
Leslie Chou <chouyuchien@gmail.com>
2023-05-08 13:53:05 -0700

    modified user, spots, and spotImages seeder files to add more data

--- backend/db/seeders/20230417234930-review.js
+++ backend/db/seeders/20230417234930-review.js
@@ -21,19 +21,19 @@ module.exports = {
     return queryInterface.bulkInsert(options, [
       {
         spotId: 1,
-        userId: 1,
+        userId: 2,
         review: 'The house was nice and quaint. The furnace kept us nice and warm.',
         stars: 4.5,
       },
       {
         spotId: 2,
-        userId: 2,
+        userId: 3,
         review: 'It was delightful being surrounded by nature in such a cozy space. Was a really special weekend, would stay here again.',
         stars: 4.8,
       },
       {
         spotId: 3,
-        userId: 3,
+        userId: 1,
         review: 'Such a beautiful escape. Beautiful views from the room & the outdoor shower was the best ever!',
         stars: 5,
       },
