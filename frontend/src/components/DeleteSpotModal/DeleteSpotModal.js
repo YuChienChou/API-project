79d9fafdd53daa4f345d5b9bfdda1e48f24b3aaf
Leslie Chou <chouyuchien@gmail.com>
2023-05-11 17:13:01 -0700

    modified DeleteSpotModal for catch data errors

--- frontend/src/components/DeleteSpotModal/DeleteSpotModal.js
+++ frontend/src/components/DeleteSpotModal/DeleteSpotModal.js
@@ -1,10 +1,12 @@
 import { useDispatch, useSelector } from "react-redux";
+import { useState } from 'react';
 import { deletSpotThunk } from "../../store/spots";
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 import { useModal } from '../../context/Modal';
 
 
 const DeleteSpotModal = () => {
+    const [errors, setErrors] = useState();
     const {spotId} = useParams();
     const spot = useSelector((state) => state.spots[spotId])
     const dispatch = useDispatch();
@@ -12,7 +14,14 @@ const DeleteSpotModal = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        return dispatch(deletSpotThunk(spot.id)).then(closeModal)
+        return dispatch(deletSpotThunk(spot.id))
+        .then(closeModal)
+        .catch(async (res) => {
+            const data = await res.json();
+            if(data && data.errors) { 
+                setErrors(data.errors);
+            }
+        });
     }
 
     return (
