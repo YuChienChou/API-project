fca9ba6d9f623eca1b09cdce3ec1a63b5262c0d1
Leslie Chou <chouyuchien@gmail.com>
2023-06-02 22:42:37 -0700

    refactor SpotReview and activated create review model

--- frontend/src/components/Reviews/SpotReviews.js
+++ frontend/src/components/Reviews/SpotReviews.js
@@ -2,15 +2,18 @@ import { useEffect } from "react";
 import { useParams } from 'react-router-dom'
 import { useDispatch, useSelector } from "react-redux"
 import { loadReviewsThunk } from "../../store/reviews";
+import CreateReviewModal from "../CreateReviewModal/CreateReviewModal";
+import OpenModalMenuItem from "../Navigation/OpenModalMenuItem";
 import './SpotReviews.css';
 
 
-const SpotReviews = () => {
+const SpotReviews = ({ spot }) => {
     const { spotId } = useParams();
     const reviewsStore = useSelector((state) => state.reviews);
     // console.log("reviewsStore in spotreviews: ", reviewsStore);
     const reviews = Object.values(reviewsStore);
-    const user = useSelector((state) => state.session.user)
+    console.log("reviews in SpotReviews: ", reviews);
+    const user = useSelector((state) => state.session.user);
     // console.log("reviews in reviewindex: ", reviews);
     const dispatch = useDispatch();
 
@@ -18,11 +21,43 @@ const SpotReviews = () => {
         dispatch(loadReviewsThunk(spotId));
     }, [dispatch, spotId]);
 
-    if(!reviews) return null;
+    if(!reviews.length) return <></>;
 
-    if(!user) {
+    // if(!user) {
         return (
             <>
+               <div> 
+                {(()=> {
+                    if(spot.numReviews === 0) { 
+                        return <h3 className='spot-review'><i className="fa-solid fa-star"></i>New</h3>
+                    } else if (spot.numReviews === 1) {
+                        return <h3 className='spot-review'><i className="fa-solid fa-star"></i>{spot.aveStarRating} - {spot.numReviews} review</h3>
+                    }else {
+                        return <h3 className='spot-review'><i className="fa-solid fa-star"></i>{spot.aveStarRating} - {spot.numReviews} reviews</h3>
+                    }
+                })()}
+            </div>
+            <div>
+                {(() => {
+                    if(!user) {
+                        return <p>Please log in to leave a review</p>
+                    } else if (spot.numReviews === 0 && user && user.id !== spot.ownerId) {
+                        return <p>Be the first to post a review!</p>
+                    } 
+                })()}
+            </div>
+            <div>
+                {(() => {
+                    if(user && user.id !== spot.ownerId) {
+                        return <button id='post-review-button'>
+                        <OpenModalMenuItem
+                            modalComponent={<CreateReviewModal spot={spot} user={user}/>}
+                            itemText='Post Your Review' 
+                            
+                            /></button>
+                    }
+                })()}
+            </div>
             <div>
                 
                 <ul>
@@ -31,40 +66,19 @@ const SpotReviews = () => {
                         id='single-spot-review'
                     >
                         <h4>{review.User.firstName}</h4>
-                        <p>{review.createdAt}</p>
+                        <p>{review.createdAt.split("-")[1]} {review.createdAt.split("-")[0]}</p>
                         <p>{review.review}</p>
+                        {(() => {
+                        if(review.userId === user.id) {
+                            return <button>Delete Review</button>
+                        }
+                        })()}
                     </li>
-                   ))}
+                ))}
                 </ul>
             </div>
             </>
         )
-    } else {
-        return (
-        <>
-        <div>
-            
-            <ul>
-               {reviews.map((review) => (
-                <li key={review.id}
-                    id='single-spot-review'
-                >
-                    <h4>{review.User.firstName}</h4>
-                    <p>{review.createdAt}</p>
-                    <p id='single-spot-review'>{review.review}</p>
-                    {(() => {
-                        if(review.ownerId === user.id) {
-                            return <button>Delete Review</button>
-                        }
-                    })()}
-                </li>
-               ))}
-            </ul>
-        </div>
-        </>
-    )
-    }
-
     
 };
 
