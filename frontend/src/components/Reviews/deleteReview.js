d3d02ebd64bc083794bfcee9b9776bc16c90ba44
Leslie Chou <chouyuchien@gmail.com>
2023-06-05 10:54:36 -0700

    modified code for deleting review for bug in live server

--- frontend/src/components/Reviews/deleteReview.js
+++ frontend/src/components/Reviews/deleteReview.js
@@ -1,17 +1,21 @@
 import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import { fetchDetailedSpotThunk } from '../../store/spots';
-import { thunkRemoveReview, loadReviewsThunk } from '../../store/reviews';
+import { thunkRemoveReview } from '../../store/reviews';
 
 
 export default function DeleteReview ({ spot, review }) {
 
+    console.log("review in DeleteReview: ", review)
+
     const dispatch = useDispatch();
+    const history =  useHistory();
 
     const handleReviewDelete = () => {
-        console.log("review id in handReviewDele");
+        console.log("review id in handReviewDele: ", review.id);
         dispatch(thunkRemoveReview(review.id));
-        // dispatch(loadReviewsThunk());
         dispatch(fetchDetailedSpotThunk(spot.id));
+        history.push(`/spots/${spot.id}`);
     };
 
     return (
